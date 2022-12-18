b206bd70bec8e29392e0bf67b8309d856ba60744
Lucas Coelho <58010418+LucasVinicius314@users.noreply.github.com>
2022-12-18 15:40:41 -0300

    Update gate.dart

--- lib/models/gate.dart
+++ lib/models/gate.dart
@@ -2,6 +2,8 @@ import 'package:logic_gates/models/node.dart';
 
 enum GateType {
   and,
+  nand,
+  nor,
   or,
 }
 
@@ -13,6 +15,26 @@ class Gate extends Node {
   bool _and() {
     for (final node in previous) {
       if (!node.evaluate()) {
+        return false;
+      }
+    }
+
+    return true;
+  }
+
+  bool _nand() {
+    for (final node in previous) {
+      if (!node.evaluate()) {
+        return true;
+      }
+    }
+
+    return false;
+  }
+
+  bool _nor() {
+    for (final node in previous) {
+      if (node.evaluate()) {
         return false;
       }
     }
@@ -35,6 +57,10 @@ class Gate extends Node {
     switch (gateType) {
       case GateType.and:
         return _and();
+      case GateType.nand:
+        return _nand();
+      case GateType.nor:
+        return _nor();
       case GateType.or:
         return _or();
     }
